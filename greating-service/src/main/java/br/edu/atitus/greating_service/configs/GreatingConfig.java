ece4def2aa480597cd00ace804cfaa9115a7d527
HENRIQUE PANISSON AGOSTINETO <1136301@ATITUS.LOCAL>
2025-04-11 21:25:29 -0300

    aula 07

---
+++ greating-service/src/main/java/br/edu/atitus/greating_service/configs/GreatingConfig.java
@@ -0,0 +1,24 @@
+package br.edu.atitus.greating_service.configs;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greating-service")
+public class GreatingConfig {
+
+	private String greating;
+	public String getGreating() {
+		return greating;
+	}
+	public void setGreating(String greating) {
+		this.greating = greating;
+	}
+	public String getDefaultName() {
+		return defaultName;
+	}
+	public void setDefaultName(String defaultName) {
+		this.defaultName = defaultName;
+	}
+	private String defaultName;
+}
